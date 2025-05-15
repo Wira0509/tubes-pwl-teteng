e258967fb571510936657a9ead123ade1dc73ac8
Wira0509 <wirahrpratama@gmail.com>
2025-05-15 19:15:50 +0700

    Penghapusan login menggunakan fb,gmail, dan linkedin

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -269,7 +269,7 @@
       <div class="form-container sign-up-container">
         <form action="#">
           <h1>Create Account</h1>
-          <div class="social-container">
+          <!-- <div class="social-container">
             <a
               href="https://facebook.com"
               class="social"
@@ -296,7 +296,7 @@
             </a>
           </div>
 
-          <span>or use your email for registration</span>
+          <span>or use your email for registration</span> -->
           <input type="text" placeholder="Name" />
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
@@ -306,12 +306,12 @@
       <div class="form-container sign-in-container">
         <form action="#">
           <h1>Sign in</h1>
-          <div class="social-container">
+          <!-- <div class="social-container">
             <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
             <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
             <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
           </div>
-          <span>or use your account</span>
+          <span>or use your account</span> -->
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
           <a href="#">Forgot your password?</a>
