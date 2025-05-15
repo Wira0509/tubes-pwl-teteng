6fa2740c263c48ff1c0a1182850a7947370dc67d
Wira0509 <wirahrpratama@gmail.com>
2025-05-15 14:35:37 +0700

    Sedikit penambahan pada tampilan home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,5 +8,11 @@
 <body>
     Hello, this is the home page.
     <p>Welcome to our website!</p>
+
+    <p>Here are some links to other pages:</p>
+    <ul>
+        <li><a href="">About Us</a></li>
+        <li><a href="">Contact Us</a></li>
+        <li><a href="">Our Services</a></li>
 </body>
 </html>
