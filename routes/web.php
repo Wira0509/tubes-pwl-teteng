4a4cbc9e42a44f213b660238583a016948af5ee9
aldrvk <aldriknoelsianipar@gmail.com>
2025-05-19 14:18:05 +0700

    Menyelesaikan halaman Home, About, dan Our Team

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/about', function(){
+    return view('about');
+})->name('about');
+
+Route::get('/ourteam', function(){
+    return view('ourteam');
+})->name('ourteam');
+
+Route::get('/login', function () {
     return view('login');
 });
