9d2e2b17ab4023bcd65500bbb09f45f892bc44db
Rafi Andara Nasution <nasutionnrafi@gmail.com>
2025-06-12 21:23:56 +0700

    membuat halaman user dashboard, grafik, mantapla

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Illuminate\Support\Carbon; 
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
+use App\Models\Transaction;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatsOverview extends BaseWidget
+{
+    use InteractsWithPageFilters;
+    protected function getStats(): array
+    {
+        
+        $firstDate = Transaction::orderBy('date_transaction')->value('date_transaction');
+
+        $startDate = isset($this->filters['startDate']) && $this->filters['startDate']
+            ? Carbon::parse($this->filters['startDate'])
+            : ($firstDate ? Carbon::parse($firstDate) : now()->startOfMonth());
+
+        $endDate = isset($this->filters['endDate']) && $this->filters['endDate']
+            ? Carbon::parse($this->filters['endDate'])
+            : now();
+
+        $pemasukan = Transaction::incomes()
+        ->whereBetween('date_transaction', [$startDate, $endDate])
+        ->sum('amount');
+        $pengeluaran = Transaction::expenses()
+        ->whereBetween('date_transaction', [$startDate, $endDate])
+        ->sum('amount');
+
+     return [
+        Stat::make('Pemasukan', $pemasukan)
+            ->description('32k increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up')
+            ->color('success'),
+        Stat::make('Pengeluaran', $pengeluaran)
+            ->description('7% increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-down')
+            ->color('danger'),
+        Stat::make('Selisih', $pemasukan - $pengeluaran)    
+            ->description('3% increase')
+            ->descriptionIcon('heroicon-m-arrow-trending-up')
+            ->color('success'),
+    ];
+    }
+}
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $defaultCategories = [
+            [
+                'name' => 'Food',
+                'is_expense' => true,
+                'image' => 'food.png',
+            ],
+            [
+                'name' => 'Transport',
+                'is_expense' => true,
+                'image' => 'transport.png',
+            ],
+            [
+                'name' => 'Salary',
+                'is_expense' => false,
+                'image' => 'salary.png',
+            ],
+            [
+                'name' => 'Entertainment',
+                'is_expense' => true,
+                'image' => 'entertainment.png',
+            ],
+        ];
+
+        foreach ($defaultCategories as $category) {
+            Category::updateOrCreate(
+                ['name' => $category['name']],
+                $category
+            );
+        }
+    }
+}
